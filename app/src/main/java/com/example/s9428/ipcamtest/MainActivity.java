4d4feaf6f78c5ad541ae9abdc4a223a61013e25d
s94285 <s94285@yahoo.com.tw>
2017-01-04 20:37:43 +0800

    It looks like I did it.

--- app/src/main/java/com/example/s9428/ipcamtest/MainActivity.java
+++ app/src/main/java/com/example/s9428/ipcamtest/MainActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.github.niqdev.mjpeg.DisplayMode;
+import com.github.niqdev.mjpeg.Mjpeg;
+import com.github.niqdev.mjpeg.MjpegView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TIMEOUT = 5;
+
+    MjpegView mjpegView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mjpegView = (MjpegView)findViewById(R.id.mjpegViewDefault);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fab.setOnClickListener(view->
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                        .setAction("Action", null).show());
+    }
+
+    private void loadIpCam() {
+        try{
+            /*
+            Mjpeg.newInstance()
+                .credential("USERNAME", "PASSWORD")
+                .open("IPCAM_URL.mjpg", TIMEOUT)
+                .subscribe(inputStream -> {
+                    mjpegView.setSource(inputStream);
+                    mjpegView.setDisplayMode(DisplayMode.BEST_FIT);
+                    mjpegView.showFps(true);
+                });*/
+        Mjpeg.newInstance()
+                .open("http://192.168.2.150:8080/video", TIMEOUT)
+                .subscribe(inputStream -> {
+                    mjpegView.setSource(inputStream);
+                    mjpegView.setDisplayMode(DisplayMode.BEST_FIT);
+                    mjpegView.showFps(true);
+                },(throwable)->{
+                    Log.e("Error: ",throwable.toString());
+                    throwable.printStackTrace();
+                });}
+        catch (Throwable onError){
+            onError.printStackTrace();
+        }
     }
 
     @Override
@@ -48,5 +81,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        mjpegView.stopPlayback();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Runnable runnable = () ->{
+            try{
+                loadIpCam();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        };
+        new Thread(runnable).start();
+        super.onResume();
     }
 }
